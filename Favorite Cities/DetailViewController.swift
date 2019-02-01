e95e2705e8f85a296ead73e44a28fd9a9f8cdc1b
Emily Kirk <ekirk@emilys-macbook-air-73.local>
2019-02-01 11:39:43 -0600

    Connected UIObjects to storyboard

--- Favorite Cities/DetailViewController.swift
+++ Favorite Cities/DetailViewController.swift
@@ -9,32 +9,48 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
+    
+    @IBOutlet weak var cityTexField: UITextField!
+    
+    @IBOutlet weak var stateTextField: UITextField!
+    
+    @IBOutlet weak var populationTextField: UITextField!
+    
+    @IBOutlet weak var flagView: UIImageView!
+    
+    var detailItem: City? {
+        didSet {
+            // Update the view.
+            configureView()
         }
-    }
-
+        
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
     }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
+  
     }
 
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let city = self.detailItem {
+            if cityTexField != nil {
+                cityTexField.text = city.name
+                stateTextField.text = city.state
+                populationTextField.text = String(city.population)
+                flagView.image = UIImage(data: city.image)
+            }
+        }
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        if let city = self.detailItem {
+            city.name = cityTexField.text!
+            city.state = stateTextField.text!
+            city.population = Int(populationTextField.text!)!
+        }
+    }
 
 }
 
